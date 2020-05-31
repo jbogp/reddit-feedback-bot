f736ae3820ce4d21c5a52252d77af80b85d88054
Jean-Baptiste Pettit <jb.pettit@gmail.com>
2020-05-31 17:50:52 +0200

    better indent

--- bot_config.py
+++ bot_config.py
@@ -4,18 +4,18 @@ import config
 
 @dataclass
 class BotConfig:
-	''' Class to store all bot applicationconfiguration '''
-	score_needed: int # Score needed to be able to post
-	look_back_posts: int # Number of posts to look back each time the bot runs
-	minimum_comment_length: int
-	filter_regex: List[str] # List of regex that if matched with the submission URL, will be filtered by the bot
+  ''' Class to store all bot applicationconfiguration '''
+  score_needed: int # Score needed to be able to post
+  look_back_posts: int # Number of posts to look back each time the bot runs
+  minimum_comment_length: int
+  filter_regex: List[str] # List of regex that if matched with the submission URL, will be filtered by the bot
 
-	@classmethod
-	def from_config(cls, file='./config/config.cfg'):
-		cfg = config.Config(file)
-		return cls(
-			cfg['score_needed'],
-			cfg['look_back_posts'],
-			cfg['minimum_comment_length'],
-			cfg['filter_regex']
-		)
+  @classmethod
+  def from_config(cls, file='./config/config.cfg'):
+    cfg = config.Config(file)
+    return cls(
+      cfg['score_needed'],
+      cfg['look_back_posts'],
+      cfg['minimum_comment_length'],
+      cfg['filter_regex']
+    )
--- bot_credentials.py
+++ bot_credentials.py
@@ -3,24 +3,24 @@ import config
 
 @dataclass
 class BotCredentials:
-	''' Class to store all the login/auth needed for the bot '''
-	client_id: str # Reddit application client_id
-	client_secret: str # Reddit application client_secret
-	user_agent: str # Reddit application user_agent
+  ''' Class to store all the login/auth needed for the bot '''
+  client_id: str # Reddit application client_id
+  client_secret: str # Reddit application client_secret
+  user_agent: str # Reddit application user_agent
 
-	bot_username: str # Username associated with the bot and the application
-	bot_password: str # Password for the bot's reddit account
+  bot_username: str # Username associated with the bot and the application
+  bot_password: str # Password for the bot's reddit account
 
-	subreddit: str # Subreddit to moderate
+  subreddit: str # Subreddit to moderate
 
-	@classmethod
-	def from_config(cls, file='./config/credentials.cfg'):
-		credentials_cfg = config.Config(file)
-		return cls(
-			credentials_cfg['client_id'],
-			credentials_cfg['client_secret'],
-			credentials_cfg['user_agent'],
-			credentials_cfg['bot_username'],
-			credentials_cfg['bot_password'],
-			credentials_cfg['subreddit']
-		)
+  @classmethod
+  def from_config(cls, file='./config/credentials.cfg'):
+    credentials_cfg = config.Config(file)
+    return cls(
+      credentials_cfg['client_id'],
+      credentials_cfg['client_secret'],
+      credentials_cfg['user_agent'],
+      credentials_cfg['bot_username'],
+      credentials_cfg['bot_password'],
+      credentials_cfg['subreddit']
+    )
